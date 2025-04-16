dd6c8d18b7dab3fa8682b3f93526803a58a00ce9
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-04-16 14:05:35 +0200

    Make Spring Boot manage docker

--- backend/src/main/java/com/techsage/banking/controllers/HelloController.java
+++ backend/src/main/java/com/techsage/banking/controllers/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/yello")
     public String hello() {
-        return "Bye world";
+        return "Hi world";
     }
 }
