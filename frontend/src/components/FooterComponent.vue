baedf818fc8ecd687a9607129a462a2ed219d88e
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-04-28 14:48:19 +0200

    es lint fix

--- frontend/src/components/FooterComponent.vue
+++ frontend/src/components/FooterComponent.vue
@@ -1,9 +1,10 @@
 <template>
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <span><img src="/images/TechSageCapitalLogo-Transparent.png" alt="TechSage Capital" />Copyright &copy; 2025 TechSage Capital</span>
+        <span
+            ><img src="/images/TechSageCapitalLogo-Transparent.png" alt="TechSage Capital" />Copyright &copy; 2025
+            TechSage Capital</span
+        >
     </footer>
 </template>
 
-<script setup lang="ts">
-import { RouterLink } from "vue-router";
-</script>
+<script setup lang="ts"></script>
