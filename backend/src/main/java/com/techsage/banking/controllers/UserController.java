de2db692b00184dcaf60ba0dd2f94bbf69f80dc2
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-04-28 11:57:22 +0200

    all logic to search by account status

--- backend/src/main/java/com/techsage/banking/controllers/UserController.java
+++ backend/src/main/java/com/techsage/banking/controllers/UserController.java
@@ -1,4 +1,23 @@
 package com.techsage.banking.controllers;
 
+import com.techsage.banking.models.User;
+import com.techsage.banking.models.dto.UserDto;
+import com.techsage.banking.services.interfaces.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+    private final UserService userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<UserDto> getAll(@RequestParam(defaultValue = "ACTIVE") User.Status status) {
+        return userService.findAllAccountsByStatus(status);
+    }
 }
