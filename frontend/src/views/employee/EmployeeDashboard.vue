202c4d4dd02acf955ce215cf9f4dfe4e5c812194
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-04-28 14:48:33 +0200

    employee dashboard

---
+++ frontend/src/views/employee/EmployeeDashboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <div class="container py-5">
+            <h1>Employee Dashboard</h1>
+            <p>This is the employee dashboard</p>
+            <RouterLink to="/employee/customers-overview" class="btn btn-primary">Customers Overview</RouterLink>
+        </div>
+    </main>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
