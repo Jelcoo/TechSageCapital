bdf8e8cb112013dcab881c89cb39171f6e1137a4
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-04-28 14:30:20 +0200

    cors

--- backend/src/main/java/com/techsage/banking/controllers/UserController.java
+++ backend/src/main/java/com/techsage/banking/controllers/UserController.java
@@ -3,12 +3,14 @@ package com.techsage.banking.controllers;
 import com.techsage.banking.models.User;
 import com.techsage.banking.models.dto.UserDto;
 import com.techsage.banking.services.interfaces.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173", maxAge = 3600)
 @RestController
 public class UserController {
     private final UserService userService;
