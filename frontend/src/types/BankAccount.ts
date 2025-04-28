60e16bcbacfeacf89efd495e12c4ff6ef2a2a3c7
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-04-28 13:37:40 +0200

    Fix types

--- frontend/src/types/BankAccount.ts
+++
@@ -1,8 +0,0 @@
-export interface BankAccount {
-    id: number;
-    Owner_ID: number; // Foreign key to User table
-    IBAN: string;
-    Balance: number;
-    AbsoluteMinimum: number; // Minimum balance required in the account
-    Type: string; // 'savings' | 'checking' | optional ATM
-}
