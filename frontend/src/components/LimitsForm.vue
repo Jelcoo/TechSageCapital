81f3f0cc075d90155a9ebd9df0b7c470d8bfb7f1
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-05-04 15:54:48 +0200

    limits form

---
+++ frontend/src/components/LimitsForm.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col col-md-6 col-lg-4 col-xl-3 py-4">
+                <main class="form-signin w-100 m-auto">
+
+                    <h1 class="h3 fw-normal">{{ props.header }}</h1>
+                    <p class="text-body-secondary">Please set the limits of this customer</p>
+
+                    <VeeForm v-slot="{ handleSubmit }" :validation-schema="validationSchema" as="div">
+                        <form @submit="handleSubmit($event, onSubmit)">
+                            <FormInput name="transferLimit" label="transfer limit" type="number" placeholder="100.00"
+                                :value="user?.bankAccounts ? user.transferLimit : 0" />
+                            <FormInput name="dailyTransferLimit" label="Daily transfer limit" type="number"
+                                placeholder="100.00" :value="user?.bankAccounts ? user.dailyLimit : 0" />
+                            <button class="btn btn-primary w-100 py-2 my-3" type="submit">{{ props.button }}</button>
+                        </form>
+                    </VeeForm>
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import axiosClient from '@/axios';
+import type { User } from '@/types';
+import type { AxiosError } from 'axios';
+import { onMounted, ref } from 'vue';
+import FormInput from '@/components/forms/FormInput.vue';
+import { Form as VeeForm, type GenericObject } from 'vee-validate';
+import * as yup from 'yup';
+
+const errorMessage = ref("");
+const loading = ref(false);
+const user = ref<User | null>(null);
+const emits = defineEmits(['submit']);
+const props = defineProps({
+    id: {
+        type: Number,
+        required: true
+    },
+    header: {
+        type: String,
+        required: true
+    },
+    button: {
+        type: String,
+        required: true
+    }
+});
+
+async function fetchUser() {
+    loading.value = true;
+    errorMessage.value = "";
+    try {
+        const response = await axiosClient.get<User>(`/users/getById/${props.id}`);
+        user.value = response.data;
+        if (!user.value || user.value == null) {
+            errorMessage.value = "User not found.";
+        }
+    } catch (error) {
+        errorMessage.value = (error as AxiosError).response
+            ? ((error as AxiosError).response?.data as { message?: string })?.message ?? "An unknown error occurred."
+            : "An error occurred while fetching user details. " + (error as AxiosError).message; // error.message is for debugging REMOVE LATER
+    } finally {
+        loading.value = false;
+    }
+}
+
+const validationSchema = yup.object({
+    transferLimit: yup.number().required().min(0).max(1000000),
+    dailyTransferLimit: yup.number().required().min(0).max(1000000),
+});
+
+const onSubmit = (values: GenericObject) => {
+    emits('submit', values);
+};
+
+onMounted(() => {
+    fetchUser();
+});
+
+</script>
+
+<style scoped></style>
