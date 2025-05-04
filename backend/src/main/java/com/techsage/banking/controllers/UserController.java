c60dbef7171e75313c3276ee91161daca2c62f1c
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-05-04 13:11:15 +0200

    approve endpoint

--- backend/src/main/java/com/techsage/banking/controllers/UserController.java
+++ backend/src/main/java/com/techsage/banking/controllers/UserController.java
@@ -1,14 +1,23 @@
 package com.techsage.banking.controllers;
 
+import com.techsage.banking.models.User;
+import com.techsage.banking.models.dto.BaseDto;
 import com.techsage.banking.models.dto.UserDto;
+import com.techsage.banking.models.dto.requests.ApprovalRequestDto;
+import com.techsage.banking.models.dto.responses.MessageDto;
 import com.techsage.banking.models.enums.*;
+import com.techsage.banking.services.interfaces.BankAccountService;
 import com.techsage.banking.services.interfaces.UserService;
+import jakarta.validation.Valid;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.*;
 import org.springframework.security.core.*;
 import org.springframework.security.core.context.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/users")
@@ -51,5 +60,20 @@ public class UserController extends BaseController {
     @GetMapping("/getById/{ID}")
     public UserDto getById(@PathVariable long ID) {
         return userService.getById(ID);
+    }
+
+    @PostMapping("{id}/approve")
+    @PreAuthorize("hasRole('EMPLOYEE')")
+    public ResponseEntity<BaseDto> approveUser(@PathVariable long id, @Valid @RequestBody ApprovalRequestDto approvalRequestDto) {
+        try {
+            return ResponseEntity.ok().body(userService.approveUser(id, approvalRequestDto));
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(new MessageDto(400, e.getMessage()));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(404).body(new MessageDto(404, e.getMessage()));
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(500).body(new MessageDto(500, e.getMessage()));
+        }
     }
 }
