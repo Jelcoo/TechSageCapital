dca9b2579dcfac949bb77334b6b2f7f8b776fe8d
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-05-14 13:38:04 +0200

    complete transaction logic

--- backend/src/main/java/com/techsage/banking/repositories/TransactionRepository.java
+++ backend/src/main/java/com/techsage/banking/repositories/TransactionRepository.java
@@ -1,9 +1,15 @@
 package com.techsage.banking.repositories;
 
+import com.techsage.banking.models.BankAccount;
 import com.techsage.banking.models.Transaction;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
+    @Query("SELECT coalesce(SUM(t.amount), 0) FROM Transaction t WHERE t.fromAccount = ?1 AND t.createdAt <= ?2 AND t.type = 'WITHDRAWAL'")
+    Double findSumOfTransactionsByFromAccount(BankAccount bankAccount, LocalDateTime date);
 }
