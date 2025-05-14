d97527a4ec977306c0029ba4d756b31260ea943b
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-05-14 17:13:02 +0200

    Better styling of atm

--- frontend/src/views/atm/HomeView.vue
+++ frontend/src/views/atm/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
-    <main>
-        <div class="container py-5">
-            <h1 class="display-4 fw-bold text-center mb-4">ATM</h1>
-
+    <div class="container">
+        <div class="atm-card text-center">
+            <img src="/images/TechSageCapital-Logo-ATM.png" alt="Tech Sage Captial Logo" class="mb-4"
+                style="width: 250px;">
+            <h3 class="mb-4">Welcome to TechSageATM&trade;</h3>
             <div class="mb-3">
                 <label for="status" class="form-label">Account:</label>
                 <select class="form-select" id="status" v-model="selectedAccount">
@@ -18,9 +19,8 @@
             <div v-if="selectedAccount">
                 {{ selectedAccount }}
             </div>
-
         </div>
-    </main>
+    </div>
 </template>
 
 <script lang="ts" setup>
@@ -41,3 +41,22 @@ onBeforeMount(() => {
         });
 });
 </script>
+
+<style scoped>
+.atm-card {
+    max-width: 400px;
+    margin: 80px auto;
+    padding: 30px;
+    border-radius: 15px;
+    background-color: #2c2f33;
+}
+
+.btn-atm {
+    background-color: #00cba9;
+    border: none;
+}
+
+.btn-atm:hover {
+    background-color: #00a98c;
+}
+</style>
