2a77807cee752563f832c29c9f6be7899e3157d1
Jelcoo <53396500+Jelcoo@users.noreply.github.com>
2025-05-14 16:49:50 +0200

    Return proper AuthorizationDeniedException dto

--- backend/src/main/java/com/techsage/banking/controllers/BaseController.java
+++ backend/src/main/java/com/techsage/banking/controllers/BaseController.java
@@ -1,6 +1,9 @@
 package com.techsage.banking.controllers;
 
+import com.techsage.banking.models.dto.*;
+import com.techsage.banking.models.dto.responses.*;
 import org.springframework.http.*;
+import org.springframework.security.authorization.*;
 import org.springframework.web.bind.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,5 +16,10 @@ public class BaseController {
         ex.getBindingResult().getFieldErrors().forEach(error ->
                 errors.put(error.getField(), error.getDefaultMessage()));
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(AuthorizationDeniedException.class)
+    public ResponseEntity<BaseDto> handleAuthorizationDeniedException(AuthorizationDeniedException ex) {
+        return ResponseEntity.status(403).body(new MessageDto(403, ex.getMessage()));
     }
 }
