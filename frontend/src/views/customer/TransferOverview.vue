68f5ddb1a90fd08dc5f77f597d498e1109b7f0fa
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-05-21 13:07:47 +0200

    transferoverview overhaul

--- frontend/src/views/customer/TransferOverview.vue
+++ frontend/src/views/customer/TransferOverview.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
-        <div class="container py-5">
-            <h1 class="display-4 fw-bold text-left mb-5">Transfer Overview</h1>
+        <div class="container py-5 col-6">
+            <h1 class="display-4 fw-bold text-left mb-2">Transfer Overview</h1>
             <div v-if="loading" class="text-center">
                 <div class="spinner-border text-primary" role="status">
                     <span class="visually-hidden">Loading...</span>
@@ -10,17 +10,13 @@
             <div v-if="errorMessage" class="alert alert-danger text-center">
                 {{ errorMessage }}
             </div>
-            <div class="row mb-4">
-                <div class="col-12 col-md-6 mb-4 mb-md-0">
-                    <div>
-                        <label for="amount" class="form-label">Amount:</label>
-                        <input type="number" name="amount" id="amount" class="form-control" v-model="amount">
-                    </div>
-                    <label for="fromAccount" class="form-label mt-3">From Account:</label>
+            <div class="d-flex flex-column gap-3">
+                <div>
+                    <label for="fromAccount" class="form-label mt-3">From:</label>
                     <ul class="list-group">
-                        <li class="list-group-item d-flex justify-content-between align-items-center"
+                        <li :class="['list-group-item d-flex justify-content-between align-items-center', fromAccount?.iban === account.iban ? 'list-group-item-light selected' : '']"
                             v-for="account in user?.bankAccounts" :key="account.id">
-                            <div>
+                            <div class="text-white">
                                 <div>
                                     <strong>Account Type:</strong> {{ account.type }}
                                 </div>
@@ -31,43 +27,45 @@
                                     <strong>Account Balance:</strong> {{ formatMoney(account.balance) }}
                                 </div>
                             </div>
-                            <button :class="['btn', 'btn-primary', fromAccount === account.iban ? 'btn-success' : '']"
-                                @click="selectFromAccount(account)" :disabled="toAccount === account.iban">
-                                {{ fromAccount === account.iban ? 'Selected' : 'Select' }}
+                            <button
+                                :class="['btn', 'btn-primary', fromAccount?.iban === account.iban ? 'btn-success' : '']"
+                                @click="selectFromAccount(account)" :disabled="toAccount?.iban === account.iban">
+                                {{ fromAccount?.iban === account.iban ? 'Selected' : 'Select' }}
                             </button>
                         </li>
                     </ul>
                 </div>
-                <div class="col-12 col-md-6">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="col-8">
-                            <label for="description" class="form-label">Description</label>
-                            <textarea name="description" id="description" class="form-control" v-model="description"
-                                rows="1" />
-                        </div>
-                        <button class="btn btn-primary mt-3" @click="transfer"
-                            :disabled="!fromAccount || !toAccount || amount <= 0">
-                            Transfer
-                        </button>
+                <div>
+                    <div>
+                        <label for="amount" class="form-label">Amount:</label>
+                        <input type="number" name="amount" id="amount" class="form-control" v-model="amount">
                     </div>
-                    <div class="mt-3">
-                        <label for="toAccount" class="form-label">To Account:</label>
-                        <div class="row g-2 align-items-center">
-                            <div class="col-12 col-sm-8">
-                                <input type="text" name="toAccount" id="toAccount" class="form-control w-50"
-                                    placeholder="NLxxINHOxxxxxxxxxx" v-model="toAccount">
-                            </div>
-                            <div class="col-12 col-sm-4">
-                                <button type="button" class="btn btn-primary" @click="openModal">
-                                    Search Accounts
-                                </button>
-                            </div>
+                    <div class="col-12">
+                        <label for="description" class="form-label">Description</label>
+                        <textarea name="description" id="description" class="form-control" v-model="description"
+                            style="height: 12px;" />
+                    </div>
+                </div>
+                <div v-if="fromAccount?.type != BankAccountType.SAVINGS">
+                    <div>
+                        <label for="toAccount" class="form-label">To:</label>
+                        <div class="d-flex align-items-center justify-content-between">
+                            <input type="text" name="toAccount" id="toAccount" class="form-control w-75"
+                                placeholder="NLxxINHOxxxxxxxxxx" v-model="toAccount">
+                            <button type="button" class="btn btn-primary" @click="openModal">
+                                Address book
+                            </button>
                         </div>
-                        <label for="toAccount" class="form-label mt-3">To own account:</label>
+                    </div>
+                </div>
+                <div v-else>
+                    <div>
+                        <label for="toAccount" class="form-label">To:</label>
                         <ul class="list-group">
-                            <li class="list-group-item d-flex justify-content-between align-items-center"
-                                v-for="account in user?.bankAccounts" :key="account.id">
-                                <div>
+                            <li :class="['list-group-item d-flex justify-content-between align-items-center', fromAccount?.iban === account.iban ? 'list-group-item-light selected' : '']"
+                                v-for="account in user?.bankAccounts.filter(account => account.type != BankAccountType.SAVINGS)"
+                                :key="account.id">
+                                <div class="text-white">
                                     <div>
                                         <strong>Account Type:</strong> {{ account.type }}
                                     </div>
@@ -78,63 +76,95 @@
                                         <strong>Account Balance:</strong> {{ formatMoney(account.balance) }}
                                     </div>
                                 </div>
-                                <button :class="['btn', 'btn-primary', toAccount === account.iban ? 'btn-success' : '']"
-                                    @click="selectToAccount(account)" :disabled="fromAccount === account.iban">
-                                    {{ toAccount === account.iban ? 'Selected' : 'Select' }}
+                                <button
+                                    :class="['btn', 'btn-primary', toAccount?.iban === account.iban ? 'btn-success' : '']"
+                                    @click="selectToAccount(account)" :disabled="fromAccount?.iban === account.iban">
+                                    {{ toAccount?.iban === account.iban ? 'Selected' : 'Select' }}
                                 </button>
                             </li>
                         </ul>
                     </div>
                 </div>
+                <button class="btn btn-primary mt-3" @click="transfer"
+                    :disabled="!fromAccount || !toAccount || amount <= 0">
+                    Transfer
+                </button>
             </div>
-        </div>
-        <!-- Modal -->
-        <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true"
-            ref="searchModal">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="searchModalLabel">Search bankAccounts</h5>
-                        <button type="button" class="btn-close" @click="closeModal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div>
-                            <label for="firstnameSearch" class="form-label">firstname:</label>
-                            <input type="text" name="firstnameSearch" id="firstnameSearch" class="form-control"
-                                v-model="firstName" @input="searchAccounts">
-                            <label for="lastnameSearch" class="form-label">lastname:</label>
-                            <input type="text" name="lastnameSearch" id="lastnameSearch" class="form-control"
-                                v-model="lastName" @input="searchAccounts">
+            <!-- Modal -->
+            <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="searchModalLabel" aria-hidden="true"
+                ref="searchModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="searchModalLabel">Address book</h5>
+                            <button type="button" class="btn-close" @click="closeModal" aria-label="Close"></button>
                         </div>
-                        <div class="mt-3">
-                            <ul class="list-group">
-                                <li class="list-group-item d-flex justify-content-between align-items-center"
-                                    v-for="account in searchResults" :key="account.id">
-                                    <div>
+                        <div class="modal-body">
+                            <div>
+                                <h5 class="mb-3">To own account:</h5>
+                                <ul class="list-group">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center"
+                                        v-for="account in user?.bankAccounts" :key="account.id">
                                         <div>
-                                            <strong>First Name:</strong> {{ account.firstName }}
+                                            <div>
+                                                <strong>Account Type:</strong> {{ account.type }}
+                                            </div>
+                                            <div>
+                                                <strong>IBAN:</strong> {{ account.iban }}
+                                            </div>
+                                            <div>
+                                                <strong>Account Balance:</strong> {{ formatMoney(account.balance) }}
+                                            </div>
                                         </div>
+                                        <button
+                                            :class="['btn', 'btn-primary', toAccount?.iban === account.iban ? 'btn-success' : '']"
+                                            @click="selectToAccount(account)"
+                                            :disabled="fromAccount?.iban === account.iban">
+                                            {{ toAccount?.iban === account.iban ? 'Selected' : 'Select' }}
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="mt-3">
+                                <h5 class="mb-1">Search accounts:</h5>
+                                <label for="firstnameSearch" class="form-label">Firstname:</label>
+                                <input type="text" name="firstnameSearch" id="firstnameSearch" class="form-control"
+                                    v-model="firstName" @input="searchAccounts">
+                                <label for="lastnameSearch" class="form-label">Lastname:</label>
+                                <input type="text" name="lastnameSearch" id="lastnameSearch" class="form-control"
+                                    v-model="lastName" @input="searchAccounts">
+                            </div>
+                            <div class="mt-3">
+                                <ul class="list-group">
+                                    <li class="list-group-item d-flex justify-content-between align-items-center"
+                                        v-for="account in searchResults" :key="account.id">
                                         <div>
-                                            <strong>Last Name:</strong> {{ account.lastName }}
+                                            <div>
+                                                <strong>First Name:</strong> {{ account.firstName }}
+                                            </div>
+                                            <div>
+                                                <strong>Last Name:</strong> {{ account.lastName }}
+                                            </div>
+                                            <div>
+                                                <strong>Account Type:</strong> {{ account.type }}
+                                            </div>
+                                            <div>
+                                                <strong>IBAN:</strong> {{ account.iban }}
+                                            </div>
                                         </div>
-                                        <div>
-                                            <strong>Account Type:</strong> {{ account.type }}
-                                        </div>
-                                        <div>
-                                            <strong>IBAN:</strong> {{ account.iban }}
-                                        </div>
-                                    </div>
-                                    <button
-                                        :class="['btn', 'btn-primary', toAccount === account.iban ? 'btn-success' : '']"
-                                        @click="selectToAccount(account)" :disabled="fromAccount === account.iban">
-                                        {{ toAccount === account.iban ? 'Selected' : 'Select' }}
-                                    </button>
-                                </li>
-                            </ul>
+                                        <button
+                                            :class="['btn', 'btn-primary', toAccount?.iban === account.iban ? 'btn-success' : '']"
+                                            @click="selectToAccount(account)"
+                                            :disabled="fromAccount?.iban === account.iban">
+                                            {{ toAccount?.iban === account.iban ? 'Selected' : 'Select' }}
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" @click="closeModal">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -145,7 +175,7 @@
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import axiosClient from "@/axios";
-import type { BankAccount, SearchResponseBankaccount, User } from "@/types";
+import { BankAccountType, type BankAccount, type SearchResponseBankaccount, type User } from "@/types";
 import type { AxiosError } from "axios";
 import { useRoute } from "vue-router";
 import { formatMoney } from "@/utils";
@@ -158,8 +188,8 @@ const userIdParam = route.params.id;
 const user = ref<User | null>(null);
 const errorMessage = ref("");
 const loading = ref(false);
-const fromAccount = ref("");
-const toAccount = ref("");
+const fromAccount = ref<BankAccount | null>(null);
+const toAccount = ref<BankAccount | SearchResponseBankaccount | null>(null);
 const amount = ref(0);
 const description = ref("");
 const firstName = ref("");
@@ -210,19 +240,19 @@ const searchAccounts = useDebounceFn(async () => {
 }, 500)
 
 function selectToAccount(account: BankAccount | SearchResponseBankaccount) {
-    if (toAccount.value === account.iban) {
-        toAccount.value = "";
+    if (toAccount.value?.iban === account.iban) {
+        toAccount.value = null;
     } else {
-        toAccount.value = account.iban;
+        toAccount.value = account;
     }
     closeModal();
 }
 
 function selectFromAccount(account: BankAccount) {
-    if (fromAccount.value === account.iban) {
-        fromAccount.value = "";
+    if (fromAccount.value?.iban === account.iban) {
+        fromAccount.value = null;
     } else {
-        fromAccount.value = account.iban;
+        fromAccount.value = account;
     }
 }
 
@@ -230,8 +260,8 @@ async function transfer() {
     errorMessage.value = "";
     try {
         const result = await axiosClient.post(`/transactions/create`, {
-            fromIban: fromAccount.value.replace(/\s/g, ''),
-            toIban: toAccount.value.replace(/\s/g, ''),
+            fromIban: fromAccount.value?.iban.replace(/\s/g, ''),
+            toIban: toAccount.value?.iban.replace(/\s/g, ''),
             amount: amount.value.toFixed(2),
             description: description.value
         });
@@ -254,4 +284,12 @@ onMounted(() => {
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.selected {
+    border-color: green;
+}
+
+.list-group-item {
+    border-width: 1px;
+}
+</style>
