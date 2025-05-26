f3eb9d2f5f10d2e2df5e86f365b5bb42d1b09210
iolite312 <68647023+iolite312@users.noreply.github.com>
2025-05-26 22:14:02 +0200

    forgot annotation

--- backend/src/main/java/com/techsage/banking/models/dto/requests/UpdateAbsoluteMinimumBalanceRequestDto.java
+++ backend/src/main/java/com/techsage/banking/models/dto/requests/UpdateAbsoluteMinimumBalanceRequestDto.java
@@ -1,10 +1,12 @@
 package com.techsage.banking.models.dto.requests;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class UpdateAbsoluteMinimumBalanceRequestDto {
+    @NotNull(message = "Absolute minimum balance is required")
     private BigDecimal absoluteMinimumBalance;
 }
